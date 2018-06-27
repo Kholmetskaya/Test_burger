8bde0c4ab61f8cc80ae6420a1b58ab74bfcc65a1
Jane <Bo4koJane@i.ua>
2018-06-27 14:43:13 +0300

    section menu

--- app/js/common.js
+++ app/js/common.js
@@ -8,13 +8,54 @@ $(function(){
 //Burger-menu
 
 document.addEventListener("DOMContentLoaded", function(){
-    var burgerMenu = document.querySelector(".slider-composition-burger");
-    var composition = document.querySelector(".slider-composition-info");
-   
-    burgerMenu.addEventListener('click', function() {
-        composition.classList.toggle('active');
-      });
+    var burgerMenu = document.querySelectorAll(".slider-composition-burger");
+    var composition = document.querySelectorAll(".slider-composition-info");
+    
+    burgerMenu.forEach(function(item, i, burgerMenu) {   
+        item.addEventListener('click', function() {
+            composition.forEach(function(item, i, composition){
+                item.classList.toggle('active');
+            });          
+        });
+    });
 });
+
+// Team-list section 4
+
+
   
+document.addEventListener("DOMContentLoaded", function(){
+    var teamList = document.getElementsByClassName("team-content-list-item");
+    var active = document.getElementsByClassName('active');
   
+    Array.from(teamList).forEach(function(item, i, teamList) {
+      item.addEventListener('click', function(e) {
+        var currentItem = this;       
+          if (active.length > 0 && active[0] !== currentItem) {
+          active[0].classList.remove('active'); 
+        }
+        currentItem.classList.add('active');
+       
+      });
+    });
+  });
+
+
+// Menu-list section 5
+
+document.addEventListener("DOMContentLoaded", function(){
+    var menuList = document.getElementsByClassName("menu-content-list-item");
+    var selected = document.getElementsByClassName('selected');
+  
+    Array.from(menuList).forEach(function(item, i, menuList) {
+      item.addEventListener('click', function(e) {
+        var selectedItem = this;       
+          if (selected.length > 0 && selected[0] !== selectedItem) {
+            selected[0].classList.remove('selected'); 
+        }
+        selectedItem.classList.add('selected');      
+      });
+    });
+  });
+
 
